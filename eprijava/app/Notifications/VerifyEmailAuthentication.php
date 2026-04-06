02253529f8a134dada7c57671eb68b72f8893d17
marko.radovanovic <mradovanovic@msn.com>
2026-04-06 12:40:19 +0200

    Move contact email to env variable

--- eprijava/app/Notifications/VerifyEmailAuthentication.php
+++ eprijava/app/Notifications/VerifyEmailAuthentication.php
@@ -13,7 +13,7 @@ class VerifyEmailAuthentication extends BaseNotification
             ->subject('Е-пријава двофакторска аутентификација')
             ->greeting('Покушавате да се пријавите на портал Е-пријаве.')
             ->line('Ваш код за пријаву је: **' . $this->code . '**')
-            ->line('Уколико то нисте били Ви, молимо Вас да контактирате Пореску управу на konkursi07@purs.gov.rs.')
+            ->line('Уколико то нисте били Ви, молимо Вас да контактирате Пореску управу на ' . env('CONTACT_EMAIL', 'konkursi07@purs.gov.rs') . '.')
             ->line('Овај код истиче за ' . ($this->codeExpiryMinutes === 1 ? 'минут' : $this->codeExpiryMinutes . ' минута') . '.')
             ->salutation('Срдачан поздрав,<br>Пореска управа');
     }
