01a0363c4abd8a2dc9816736070a3c0b2c3feaf2
marko.radovanovic <mradovanovic@msn.com>
2026-04-28 12:28:48 +0200

    Add Filament theme CSS and user ID badge component

---
+++ eprijava/resources/views/filament/components/user-id-badge.blade.php
@@ -0,0 +1,3 @@
+<div class="px-4 text-xs text-gray-500 dark:text-gray-400 whitespace-nowrap">
+    Ваш идентификациони број је: <strong class="font-semibold text-gray-700 dark:text-gray-200">{{ $userId }}</strong>
+</div>
